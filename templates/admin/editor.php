264e4cb57720f68aa0a9c4ba6baec47540496b58
Joe Simpson <headbangerkenny@googlemail.com>
2011-04-03 16:54:21 +0100

    Start of customizing the date for publishing, however in a nice way. Graphic is self-made

--- templates/admin/editor.php
+++ templates/admin/editor.php
@@ -1,6 +1,24 @@
 <div id="main-post" class="box left-col">
 	<form method="post" action="post-editor.php?post=<?php echo $_GET['post']; ?>">
-		<input type="text" name="title" value="<?php echo $data->title; ?>" />
+		<div id="date-input">
+			<select id="day">
+				<option>1st</option>
+				<option>2nd</option>
+				<option>3rd</option>
+			</select>
+			<select id="month">
+				<option>January</option>
+				<option>Febrauary</option>
+				<option>March</option>
+				<option>April</option>
+				<option>May</option>
+				<option></option>
+			</select>
+		</div>
+		<div id="title" class="pad10 most left">
+			<input type="text" name="title" value="<?php echo $data->title; ?>" />
+		</div>
+		<span class="clear"></span>
 		<textarea name="body" style="width:100%; min-height:400px;padding:6px;"><?php echo $data->body; ?></textarea>
 		<input type="submit" value="Save Post" />
 	</form>
